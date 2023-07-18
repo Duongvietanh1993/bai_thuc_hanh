6ea7a032cef9d3a6b64997fd29250aac44835721
Việt Anh Dương <kts.duongvietanh@gmail.com>
2023-07-18 18:52:54 +0700

    session 7

---
+++ session_7/js/main.js
@@ -0,0 +1,36 @@
+//Bài 1
+
+/*let math = parseInt(prompt("Nhập số điểm toán: "));
+let physical = parseInt(prompt("Nhập số điểm lý: "));
+let chemistry = parseInt(prompt("Nhập số điểm hóa: "));
+let sum = (math + physical + chemistry);
+
+document.write(`Điểm tổng 3 môn là: ${sum} .Điểm trung bình 3 môn là: ${sum/3}`)*/
+
+//Bài 2
+
+/*let tempC = parseInt(prompt("Nhập vào nhiệt độ C: "))
+let tempF = tempC * 1.8 + 32;
+document.write(`${tempC} độ C sẽ bằng ${tempF} độ F`)*/
+
+//Bài 3
+/*
+const PI = 3.14;
+let radius=parseInt(prompt("Nhập vào bán kính hình tròn: "))
+let area=PI * Math.pow(radius,2);
+
+document.write(`Diện tích hình tròn là ${area}`)
+*/
+
+//Bài 4
+/*const PI = 3.14;
+let radius=parseInt(prompt("Nhập vào bán kính hình tròn: "))
+let perimeter=PI * (radius*2);
+
+document.write(`Diện tích hình tròn là ${perimeter}`)*/
+
+/*
+let x=5;
+let y ="2";
+let z = x/y;
+console.log(z)*/
